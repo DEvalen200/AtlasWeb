58047692c9872d8acc29a1a9663c1c34e9950ba1
DEvalen200 <devalen200@gmail.com>
2022-09-13 13:26:39 +0200

    Fixed link preview spaces
    Changes spaces in meta tag URL to %20

--- atlas-web/pages/index.js
+++ atlas-web/pages/index.js
@@ -16,10 +16,11 @@ export default function Home() {
         <meta property="og:url" content="https://atletismeatlas.es"/>
         <meta property="og:title" content="CA Atlas &#127760;"/>
         <meta property="og:description" content="Punto de partida para unos nuevos ilímites."/>
-        <meta property="og:image" content="https://atletismeatlas.es/Link Preview/OG_Preview.png"/>
+        <meta property="og:image" content="https://atletismeatlas.es/Link%20Preview/OG_Preview.png"/>
         <meta property="og:site_name" content="CA Atlas &#127760;"/>
         {/*<!-- Twitter -->*/}
         <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:image" content="https://atletismeatlas.es/Link%20Preview/Twitter_Preview.png"/>
 
       </Head>
     </div>
