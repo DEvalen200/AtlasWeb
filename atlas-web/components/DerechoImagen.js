93b437d90632f52b2db2db84e5a2e41ff6f9963e
DEvalen200 <devalen200@gmail.com>
2022-09-19 17:11:19 +0200

    Fixed Derecho And Politicas React DisplayName

--- atlas-web/components/DerechoImagen.js
+++ atlas-web/components/DerechoImagen.js
@@ -1,78 +1,82 @@
-export default () => (
-  <div>
-    Mediante la aceptación de la política de cesión de derechos de imagen, la
-    persona firmante, acepta que <br />
-    <br />
-    <ul style={{ listStyleType: "disc", listStylePosition: "inside" }}>
-      <li>
-        Las imágenes, grabaciones audiovisuales y datos personales recogidos
-        durante las diferentes actividades donde se pueda identificar al
-        deportista, puedan ser publicadas en cualquier medio
-        gráfico/audiovisual/digital correspondiente a la entidad, de difusión
-        pública no comercial y revistas de ámbito deportivo, con objeto de
-        informar para el fomento y difusión de diferentes actividades.
-        <br />
-        <br />
-      </li>
-      <li>
-        Las imágenes, grabaciones audiovisuales y datos personales recogidos
-        durante las diferentes actividades en las que se pueda identificar al
-        deportista puedan ser publicadas en la web del Club.
-        <br />
-        <br />
-      </li>
-      <li>
-        Las imágenes, grabaciones audiovisuales y datos personales recogidos
-        durante las diferentes actividades en las que se pueda identificar al
-        deportista sean publicadas en las redes sociales del Club.
-        <br />
-        <br />
-      </li>
-      <li>
-        Que los datos personales, las grabaciones audiovisuales y las imágenes
-        recogidas durante las diferentes actividades organizadas por el club o
-        publicadas en las plataformas o aplicaciones utilizadas en el Club donde
-        se pueda identificar al deportista, puedan ser distribuidas a los padres
-        o tutores de los deportistas que hayan participado en cada actividad
-        concreta.
-        <br />
-        <br />
-      </li>
-    </ul>
-    <br />
-    <h1 style={{ textAlign: "center" }}>
-      <b>
-        PROHIBICIÓN DE GRABACIÓN DE IMÁGENES O VOZ DE MONITORES/ENTRENADORES Y
-        PERSONAL DEL CLUB
-      </b>
-    </h1>
-    <br />
-    Se advierte expresamente a la comunidad deportiva que está prohibido: <br />
-    <br />
-    1. La grabación de la imagen y/o voz, por parte de los padres, tutores
-    legales, familiares y deportistas, de los monitores/entrenadores o personal
-    del Club durante el transcurso de reuniones presenciales, conversaciones
-    telefónicas o videoconferencias, así como su posterior comunicación y/o
-    difusión mediante páginas web, redes sociales y aplicaciones de mensajería
-    instantánea. <br />
-    <br />
-    2. Grabaciones ocultas de imagen y sonido en diferentes áreas del Club de la
-    imagen y/o voz de monitores/entrenadores, personal del Club y deportistas,
-    así como su posterior comunicación a terceros y/o difusión mediante páginas
-    web, redes sociales y aplicaciones de mensajería instantánea. <br />
-    <br />
-    3. Grabar o realizar capturas de pantalla desde el ordenador u otro
-    dispositivo de las actividades online que se imparten por parte del Club,
-    así como su descarga, difusión, distribución o difusión por cualquier medio,
-    especialmente mediante páginas web, redes sociales, aplicaciones de
-    mensajería o servicios dedicados a compartir apuntes. <br />
-    <br />
-    Estas conductas no sólo no serán permitidas por el Club, aplicando
-    rigurosamente el régimen disciplinario previsto en la normativa interna del
-    Club y la legislación de aplicación, sino que también podrán ser objeto de
-    actuaciones realizadas directamente por la Junta Directiva, personal del
-    Club y deportistas afectados, ya que se ven afectados sus derechos
-    fundamentales a la protección de datos personales y al honor, la intimidad y
-    la propia imagen.
-  </div>
-);
+export default function DerechoImagen() {
+  return (
+    <div>
+      Mediante la aceptación de la política de cesión de derechos de imagen, la
+      persona firmante, acepta que <br />
+      <br />
+      <ul style={{ listStyleType: "disc", listStylePosition: "inside" }}>
+        <li>
+          Las imágenes, grabaciones audiovisuales y datos personales recogidos
+          durante las diferentes actividades donde se pueda identificar al
+          deportista, puedan ser publicadas en cualquier medio
+          gráfico/audiovisual/digital correspondiente a la entidad, de difusión
+          pública no comercial y revistas de ámbito deportivo, con objeto de
+          informar para el fomento y difusión de diferentes actividades.
+          <br />
+          <br />
+        </li>
+        <li>
+          Las imágenes, grabaciones audiovisuales y datos personales recogidos
+          durante las diferentes actividades en las que se pueda identificar al
+          deportista puedan ser publicadas en la web del Club.
+          <br />
+          <br />
+        </li>
+        <li>
+          Las imágenes, grabaciones audiovisuales y datos personales recogidos
+          durante las diferentes actividades en las que se pueda identificar al
+          deportista sean publicadas en las redes sociales del Club.
+          <br />
+          <br />
+        </li>
+        <li>
+          Que los datos personales, las grabaciones audiovisuales y las imágenes
+          recogidas durante las diferentes actividades organizadas por el club o
+          publicadas en las plataformas o aplicaciones utilizadas en el Club
+          donde se pueda identificar al deportista, puedan ser distribuidas a
+          los padres o tutores de los deportistas que hayan participado en cada
+          actividad concreta.
+          <br />
+          <br />
+        </li>
+      </ul>
+      <br />
+      <h1 style={{ textAlign: "center" }}>
+        <b>
+          PROHIBICIÓN DE GRABACIÓN DE IMÁGENES O VOZ DE MONITORES/ENTRENADORES Y
+          PERSONAL DEL CLUB
+        </b>
+      </h1>
+      <br />
+      Se advierte expresamente a la comunidad deportiva que está prohibido:{" "}
+      <br />
+      <br />
+      1. La grabación de la imagen y/o voz, por parte de los padres, tutores
+      legales, familiares y deportistas, de los monitores/entrenadores o
+      personal del Club durante el transcurso de reuniones presenciales,
+      conversaciones telefónicas o videoconferencias, así como su posterior
+      comunicación y/o difusión mediante páginas web, redes sociales y
+      aplicaciones de mensajería instantánea. <br />
+      <br />
+      2. Grabaciones ocultas de imagen y sonido en diferentes áreas del Club de
+      la imagen y/o voz de monitores/entrenadores, personal del Club y
+      deportistas, así como su posterior comunicación a terceros y/o difusión
+      mediante páginas web, redes sociales y aplicaciones de mensajería
+      instantánea. <br />
+      <br />
+      3. Grabar o realizar capturas de pantalla desde el ordenador u otro
+      dispositivo de las actividades online que se imparten por parte del Club,
+      así como su descarga, difusión, distribución o difusión por cualquier
+      medio, especialmente mediante páginas web, redes sociales, aplicaciones de
+      mensajería o servicios dedicados a compartir apuntes. <br />
+      <br />
+      Estas conductas no sólo no serán permitidas por el Club, aplicando
+      rigurosamente el régimen disciplinario previsto en la normativa interna
+      del Club y la legislación de aplicación, sino que también podrán ser
+      objeto de actuaciones realizadas directamente por la Junta Directiva,
+      personal del Club y deportistas afectados, ya que se ven afectados sus
+      derechos fundamentales a la protección de datos personales y al honor, la
+      intimidad y la propia imagen.
+    </div>
+  );
+}
